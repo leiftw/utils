d8f87e0ccb7faab319a1b17a34ca800da84a1c8a
leiftw <leif@leif.tw>
2021-03-16 18:43:52 +0100

    add `ReadPUtil`

---
+++ src/ReadPUtil.hs
@@ -0,0 +1,19 @@
+module ReadPUtil where
+
+import Data.Char (isDigit)
+
+import Text.ParserCombinators.ReadP (ReadP,satisfy,(<++))
+
+
+maxMany :: ReadP a -> ReadP [a]
+maxMany p = maxMany1 p <++ return []
+-- common usage from my AP class '15-'16
+maxMany1 :: ReadP a -> ReadP [a]
+maxMany1 p = (:) <$> p <*> (maxMany p)
+
+
+int :: ReadP Int
+int = maxMany1 (satisfy isDigit) >>= (return . read)
+
+restofline :: ReadP String
+restofline = maxMany1 $ satisfy (/='\n')
